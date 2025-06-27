904968f15181c8764fcae10bba5aee4cd89a4bd4
geddadalakshman <geddadalakshman1@gmail.com>
2025-06-27 12:21:02 +0530

    lambda files uplaoding

---
+++ lambda/config/test/test.py
@@ -0,0 +1,7 @@
+#write a function that adds two numbers and call it
+def add(a, b):
+    return a + b    
+
+# Call the function
+result = add(5, 3)
+print(f"The result of adding 5 and 3 is: {result}")
