9804b23698aa9aba51c06c9a76f6638d12e76a23
geddadalakshman <geddadalakshman1@gmail.com>
2025-10-04 20:59:56 +0530

    created new dev branch

--- lambda/demo-lambda/demo.py
+++ lambda/demo-lambda/demo.py
@@ -3,5 +3,5 @@ def lambda_handler(event, context):
     print("Hello World")
     return {
         'statusCode': 200,
-        'body': 'Hello from Lambda!'
+        'body': 'Hello from Lambda test1!'
     }
